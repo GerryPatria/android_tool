cfe735dfd167d3ed92b46d2606e685a5cdcb7c73
GerryPatria <90175229+GerryPatria@users.noreply.github.com>
2021-09-20 22:21:15 +0700

    Update Fragment TabLayout
    //TabLayout Add here if more Tab
    //Need generate More JavaFragment. (ATTENTION!) -> mainActivity

--- app/src/main/java/com/real/dbci_apk/MainActivity.java
+++ app/src/main/java/com/real/dbci_apk/MainActivity.java
@@ -1,13 +1,69 @@
 package com.real.dbci_apk;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.viewpager2.widget.ViewPager2;
+
 import android.os.Bundle;
 
+import com.google.android.material.tabs.TabLayout;
+
 public class MainActivity extends AppCompatActivity {
+
+
+    TabLayout tabLayout;
+    ViewPager2 pager2;
+    FragmentStateAdapter adapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dashbard_view);
+
+        tabLayout = findViewById(R.id.tab_layout);
+        pager2 = findViewById(R.id.view_pager2);
+
+        // Fragment
+        FragmentManager fm = getSupportFragmentManager();
+        adapter = new FragmentAdapter(fm, getLifecycle());
+        pager2.setAdapter(adapter);
+
+        //TabLayout Add here if more Tab
+        //Need generate More JavaFragment. (ATTENTION!)
+        tabLayout.addTab(tabLayout.newTab().setText("Play"));
+        tabLayout.addTab(tabLayout.newTab().setText("Tool"));
+        tabLayout.addTab(tabLayout.newTab().setText("Acc"));
+        //tabLayout.addTab(tabLayout.newTab().setText("Stat"));
+        //tabLayout.addTab(tabLayout.newTab().setText("Info"));
+
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+              pager2.setCurrentItem(tab.getPosition());
+
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+        pager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                tabLayout.selectTab(tabLayout.getTabAt(position));
+            }
+        });
+
+
     }
 }
